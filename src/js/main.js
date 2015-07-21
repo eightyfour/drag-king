d51f8c7927938350462e4a3bb78124f87bf3e01d
Alexander Garth Odell <aode@dl-iad.office.gameduell.de>
2015-07-21 11:13:49 +0200

    added some style

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,31 @@
 var canny = require('canny');
 // main js will be generated from browserify to one
 canny.add('upload', require('./c-upload.js'));
+
+var uploadBox = document.getElementById('dropzone'),
+    overClass = 'over',
+    alreadyOver = false;
+
+uploadBox.addEventListener("dragover", FileSelectHandler, false);
+uploadBox.addEventListener("dragleave", FileSelectHandler, false);
+uploadBox.addEventListener("drop", FileSelectHandler, false);
+
+function FileSelectHandler(evt){
+    var eventType = evt.type.toString(); //UGLY
+    if( events.hasOwnProperty(eventType) ){
+        events[eventType]();
+    }
+}
+
+var events = {
+    dragover: function(){
+        if(!alreadyOver){
+            uploadBox.classList.add(overClass);
+            alreadyOver = true;
+        }
+    },
+    dragleave: function(){
+        uploadBox.classList.remove(overClass);
+        alreadyOver = false;
+    }
+}
