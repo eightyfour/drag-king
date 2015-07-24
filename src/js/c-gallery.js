0c381bd17c4230e40d1be996d82ce7511a47d180
eightyfour <eightyfour@thinkuseful.de>
2015-07-25 01:50:56 +0200

    add new footer gallery - add marching ants border - add picture delete function

--- src/js/c-gallery.js
+++ src/js/c-gallery.js
@@ -2,34 +2,108 @@
  *
  * @returns {{add: Function, ready: Function}}
  */
+
+function setupZeroClipboard(imageNode, url){
+    var fullUrl = location.origin + url;
+    var client = new ZeroClipboard(imageNode);
+    client.on('ready', function(event){
+        client.on( 'copy', function(event) {
+            event.clipboardData.setData('text/plain', fullUrl);
+        });
+        client.on('aftercopy', function(event){
+            imageNode.parentNode.classList.add('copied');
+            imageNode.parentNode.setAttribute('data', url);
+            console.debug('You just copied', fullUrl);
+        })
+    });
+}
+
 var node;
 module.exports = function () {
-    function setupZeroClipboard(imageNode, url){
-        var fullUrl = location.origin + url;
-        var client = new ZeroClipboard(imageNode);
-        client.on('ready', function(event){
-            client.on( 'copy', function(event) {
-                event.clipboardData.setData('text/plain', fullUrl);
-            });
-            client.on('aftercopy', function(event){
-                imageNode.parentNode.classList.add('copied');
-                imageNode.parentNode.setAttribute('data', url);
-                console.debug('You just copied', fullUrl);
-            })
+
+    function appendImage(path) {
+        var container = document.createElement('div'),
+            imgNode = document.createElement('div'),
+            clipNode = document.createElement('div'),
+            removeBtn = document.createElement('div'),
+            myImage = new Image();
+
+
+        container.className = 'gallery-image-wrap';
+//        myImage.height="100";
+        myImage.src = path;
+        myImage.addEventListener('load', function () {
+            container.classList.add('c-loaded');
         });
+        imgNode.style.backgroundImage = "url(" + path + ")";
+        imgNode.className = 'img';
+        removeBtn.className = 'deleteBtn';
+        clipNode.className = 'copyClipNode';
+        container.appendChild(imgNode);
+        imgNode.appendChild(removeBtn);
+        imgNode.appendChild(clipNode);
+        container.appendChild(myImage);
+        node.appendChild(container);
+
+        removeBtn.setAttribute('title', 'remove this image');
+        clipNode.setAttribute('title', 'Click to copy to clip board');
+
+        // register click listener for the remove duel request
+        removeBtn.addEventListener('click', function () {
+           var xhr = new XMLHttpRequest();
+            xhr.open("GET", "/deleteFile?filename=" + path, true);
+            xhr.onload = function (e) {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        container.remove();
+                    } else {
+                        console.error(xhr.statusText);
+                    }
+                }
+            };
+            xhr.onerror = function (e) {
+                console.error(xhr.statusText);
+            };
+            xhr.send(null);
+        });
+
+        setupZeroClipboard(clipNode, path);
+    }
+
+    function addImage(imagePath){
+         // array is a object :)
+            if (typeof imagePath === 'object') {
+               imagePath.forEach(function (path) {
+                   appendImage(path);
+               });
+            } else {
+                appendImage(imagePath);
+            }
     }
     return {
         add : function (elem, attr) {
             node = elem;
         },
-        addImage : function(url){
-            var container = document.createElement('div');
-            container.className = 'container';
-            var myImage = new Image(300);
-            myImage.src = url;
-            container.appendChild(myImage);
-            node.appendChild(container);
-            setupZeroClipboard(myImage, url);
+        /**
+         * pass and array of images paths or an single path as string
+         */
+        addImage : addImage,
+        ready : function () {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "/getFiles", true);
+            xhr.onload = function (e) {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        addImage(JSON.parse(xhr.responseText));
+                    } else {
+                        console.error(xhr.statusText);
+                    }
+                }
+            };
+            xhr.onerror = function (e) {
+                console.error(xhr.statusText);
+            };
+            xhr.send(null);
         }
     }
 };
---
+++ src/js/c-uploadPostView.js
@@ -0,0 +1,39 @@
+/**
+ *
+ * @returns {{add: Function, ready: Function}}
+ */
+var node;
+module.exports = function () {
+    function setupZeroClipboard(imageNode, url){
+        var fullUrl = location.origin + url;
+        var client = new ZeroClipboard(imageNode);
+        client.on('ready', function(event){
+            client.on( 'copy', function(event) {
+                event.clipboardData.setData('text/plain', fullUrl);
+            });
+            client.on('aftercopy', function(event){
+                imageNode.parentNode.classList.add('copied');
+                imageNode.parentNode.setAttribute('data', url);
+                console.debug('You just copied', fullUrl);
+            })
+        });
+    }
+    return {
+        add : function (elem, attr) {
+            node = elem;
+        },
+        addImage : function(url){
+            var container = document.createElement('div'),
+                myImage = new Image(300);
+            container.className = 'container';
+            myImage.src = url;
+            container.appendChild(myImage);
+            node.appendChild(container);
+            setupZeroClipboard(myImage, url);
+        }
+    }
+};
+
+
+
+
