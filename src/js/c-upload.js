93808ecd59ff189e7142707ba7a91815939476b1
eightyfour <eightyfour@thinkuseful.de>
2016-02-22 00:05:43 +0100

    make possible to upload same file twice

--- src/js/c-upload.js
+++ src/js/c-upload.js
@@ -2,19 +2,25 @@ var trade = require('./trade.js'),
     brain = {
         fileInput : {
             init: function (node) {
-                node.addEventListener('change', function () {
-                    var file = this.files[0];
-                    if (file) {
-                        // send it direct after drop
-                        [].slice.call(this.files).forEach(function (file) {
-                            // instead pass  directly a array of files - so we save POST calls
-                            trade.doCall('sendFile')(file);
-                        });
-                    }
-                });
+                node.addEventListener('change', upload);
             }
         }
     };
+
+function upload() {
+    console.log('c-upload:trigger upload');
+    var file = this.files[0];
+    if (file) {
+        // send it direct after drop
+        [].slice.call(this.files).forEach(function (file) {
+            // instead pass  directly a array of files - so we save POST calls
+            trade.doCall('sendFile')(file);
+        });
+        // cleanup value otherwise file with same name can't uploaded again
+        this.value = null;
+        return false;
+    }
+}
 /**
  *
  * @returns {{add: Function, ready: Function}}
