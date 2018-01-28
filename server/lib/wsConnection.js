189f965e868437ab2efa88aa0f0b210af4c08e5d
eightyfour <eightyfour@thinkuseful.de>
2018-01-29 00:15:15 +0100

    remove delay from getDirectoryTree call

--- server/lib/wsConnection.js
+++ server/lib/wsConnection.js
@@ -31,7 +31,7 @@ const sock = shoe(function (stream) {
          * @param cb
          */
         getDirectoryTree : function (path, cb) {
-            setTimeout(() => fileHandler.getDirectoryTree(config.dirName, path, cb), 2000)
+            fileHandler.getDirectoryTree(config.dirName, path, cb)
         },
         /**
          *
